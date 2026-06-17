skoeva/aksd-sandbox

telemetry: add private-AI chokepoint with sanitizers and typed helpers
Add the telemetry module: module-private ApplicationInsights instance,
five typed track* wrappers, closed-vocabulary sanitizers and count
bucketers in schema.ts, renderer install-id and app-info helpers, and
the redux event callback that routes HeadlampEvents to trackFeature and
trackPluginsLoaded.

The plural-resource homogeneity check in extractKindFromPayload compares
raw resource.kind values before sanitizing so two distinct CRD kinds are
not falsely collapsed to the same "CustomResource" bucket.

plugins/aks-desktop/src/telemetry/index.ts
@@ -1,0 +1,206 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache 2.0.
+
+import { ApplicationInsights, type ITelemetryItem } from '@microsoft/applicationinsights-web';
+import type { AppInfo } from './appInfo';
+import {
+  type AksTier,
+  bucketNamespaceCount,
+  bucketNodeCount,
+  KNOWN_PLUGIN_IDS,
+  kubernetesMinor,
+  localeLanguage,
+  type NamespaceCountBucket,
+  type NodeCountBucket,
+  sanitizeFeatureType,
+  sanitizeKind,
+  sanitizeRegion,
+  sanitizeStatus,
+  sanitizeTier,
+} from './schema';
+import { registerReduxCallback } from './setup';
+
+// Module-private SDK handle. Only the typed track* functions below can
+// post envelopes.
+let ai: ApplicationInsights | null = null;
+
+// True once initTelemetry has run, even if construction failed. Lets us
+// skip retries on subsequent renders (e.g. StrictMode double-mount,
+// re-mount after a failed connection-string parse).
+let initAttempted = false;
+
+// Per-resource-id dedupe of headlamp.cluster-shape across re-renders.
+// Module-private (the key is never sent).
+const emittedShapeFor = new Set<string>();
+
+/** Test-only state reset. Do not call from production code. */
+export function __resetForTests(): void {
+  ai = null;
+  initAttempted = false;
+  emittedShapeFor.clear();
+}
+
+export interface SessionStartProps extends AppInfo {
+  appVersion: string;
+  /** Headlamp base version (REACT_APP_HEADLAMP_VERSION). */
+  headlampVersion: string;
+  locale: string;
+}
+
+export interface InitTelemetryOptions {
+  connectionString: string;
+  installId: string;
+  sessionProps: SessionStartProps;
+}
+
+/**
+ * Initialize App Insights and wire the redux event callback. Idempotent:
+ * the initAttempted flag survives React StrictMode double-mount.
+ *
+ * Fails closed — if the AI constructor throws, ai stays null and
+ * initAttempted stays true so we don't retry on every render.
+ */
+export function initTelemetry(opts: InitTelemetryOptions): void {
+  if (initAttempted) return;
+  initAttempted = true;
+
+  try {
+    ai = new ApplicationInsights({
+      config: {
+        connectionString: opts.connectionString,
+        disableFetchTracking: true,
+        disableAjaxTracking: true,
+        disableExceptionTracking: true,
+        disableCookiesUsage: true,
+        isStorageUseDisabled: true,
+        enableAutoRouteTracking: false,
+      },
+    });
+    ai.loadAppInsights();
+
+    // Stamp ai.user.id with the install UUID so the Azure Portal Users
+    // metric correlates by install rather than by SDK session. The id
+    // lives only as an envelope tag — never as a regular property
+    // dimension (it would otherwise be queryable/exportable as data).
+    const { installId } = opts;
+    ai.addTelemetryInitializer((envelope: ITelemetryItem) => {
+      envelope.tags = envelope.tags ?? {};
+      envelope.tags['ai.user.id'] = installId;
+    });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error('[aksd-telemetry] init failed:', e);
+    ai = null;
+    return;
+  }
+
+  trackSessionStart(opts.sessionProps);
+  registerReduxCallback();
+}
+
+export function isTelemetryInitialized(): boolean {
+  return initAttempted;
+}
+
+/** The only path from the typed wrappers to ai.trackEvent. */
+function emit(name: string, properties: Record<string, string | undefined>): void {
+  if (!ai) return;
+  const filtered: Record<string, string> = {};
+  for (const [key, value] of Object.entries(properties)) {
+    if (value !== undefined) filtered[key] = value;
+  }
+  try {
+    ai.trackEvent({ name, properties: filtered });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error('[aksd-telemetry] track failed:', name, e);
+  }
+}
+
+export function trackSessionStart(p: SessionStartProps): void {
+  emit('headlamp.session-start', {
+    appVersion: p.appVersion,
+    locale: localeLanguage(p.locale),
+    os: p.os,
+    arch: p.arch,
+    electronVersion: p.electronVersion,
+    headlampVersion: p.headlampVersion,
+  });
+}
+
+export interface ClusterShapeInput {
+  kubernetesVersion: string | null | undefined;
+  nodeCount: number | null | undefined;
+  namespaceCount: number | null | undefined;
+  region: string | null | undefined;
+  aksTier: string | null | undefined;
+}
+
+/**
+ * Emits one `headlamp.cluster-shape` per dedupeKey. No-ops when any
+ * field is null/undefined/empty or when dedupeKey was already seen --
+ * never mix Unknown with real values, never re-emit for the same
+ * cluster across re-renders.
+ */
+export function trackClusterShape(dedupeKey: string, input: ClusterShapeInput): void {
+  // Bail before touching the dedupe set when telemetry isn't initialized:
+  // otherwise a pre-init call would mark the key as seen and permanently
+  // suppress the post-init emission for the same cluster.
+  if (!ai || emittedShapeFor.has(dedupeKey)) return;
+  const {
+    kubernetesVersion: kv,
+    nodeCount: nc,
+    namespaceCount: nsc,
+    region: r,
+    aksTier: t,
+  } = input;
+  if (!kv || nc === null || nc === undefined || nsc === null || nsc === undefined || !r || !t) {
+    return;
+  }
+  emittedShapeFor.add(dedupeKey);
+  emit('headlamp.cluster-shape', {
+    provider: 'AKS',
+    kubernetesMinor: kubernetesMinor(kv),
+    nodeCountBucket: bucketNodeCount(nc) satisfies NodeCountBucket,
+    namespaceCountBucket: bucketNamespaceCount(nsc) satisfies NamespaceCountBucket,
+    region: sanitizeRegion(r),
+    aksTier: sanitizeTier(t) satisfies AksTier,
+  });
+}
+
+export interface FeatureProps {
+  feature: string;
+  status: string;
+  resourceKind?: string;
+}
+
+export function trackFeature(p: FeatureProps): void {
+  const safeFeature = sanitizeFeatureType(p.feature);
+  if (safeFeature === undefined) return;
+  emit('headlamp.feature', {
+    feature: safeFeature,
+    status: sanitizeStatus(p.status),
+    resourceKind: p.resourceKind === undefined ? undefined : sanitizeKind(p.resourceKind),
+  });
+}
+
+export function trackException(errorName: string): void {
+  emit('headlamp.exception', { errorName: errorName || 'Unknown' });
+}
+
+export interface PluginsLoadedProps {
+  totalCount: number;
+  enabledCount: number;
+  knownEnabledIds: string[];
+  thirdPartyCount: number;
+}
+
+export function trackPluginsLoaded(p: PluginsLoadedProps): void {
+  const knownOnly = p.knownEnabledIds.filter(id => KNOWN_PLUGIN_IDS.has(id));
+  emit('headlamp.plugins-loaded', {
+    totalCount: String(p.totalCount),
+    enabledCount: String(p.enabledCount),
+    knownEnabledIds: knownOnly.join(','),
+    thirdPartyCount: String(p.thirdPartyCount),
+  });
+}

plugins/aks-desktop/src/telemetry/schema.ts
@@ -1,0 +1,158 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache 2.0.
+
+/** Sanitizers and closed vocabularies for telemetry envelope properties. */
+
+export const BUILTIN_K8S_KINDS: ReadonlySet<string> = new Set([
+  // Core
+  'Pod',
+  'Service',
+  'Namespace',
+  'Node',
+  'ConfigMap',
+  'Secret',
+  'PersistentVolume',
+  'PersistentVolumeClaim',
+  'ServiceAccount',
+  'Endpoints',
+  'EndpointSlice',
+  'Event',
+  'LimitRange',
+  'ResourceQuota',
+  // Apps
+  'Deployment',
+  'StatefulSet',
+  'DaemonSet',
+  'ReplicaSet',
+  'ReplicationController',
+  // Batch
+  'Job',
+  'CronJob',
+  // Networking
+  'Ingress',
+  'IngressClass',
+  'NetworkPolicy',
+  'Gateway',
+  'HTTPRoute',
+  'GRPCRoute',
+  // Storage
+  'StorageClass',
+  'VolumeAttachment',
+  'CSIDriver',
+  'CSINode',
+  // RBAC
+  'Role',
+  'RoleBinding',
+  'ClusterRole',
+  'ClusterRoleBinding',
+  // Autoscaling
+  'HorizontalPodAutoscaler',
+  'VerticalPodAutoscaler',
+  'PodDisruptionBudget',
+  // Admission
+  'MutatingWebhookConfiguration',
+  'ValidatingWebhookConfiguration',
+  // API
+  'CustomResourceDefinition',
+  'APIService',
+  // Lease
+  'Lease',
+]);
+
+export const KNOWN_PLUGIN_IDS: ReadonlySet<string> = new Set(['aks-desktop']);
+
+/** Redux event types forwarded as `headlamp.feature` envelopes. */
+export const KNOWN_FEATURE_TYPES: ReadonlySet<string> = new Set([
+  'headlamp.delete-resource',
+  'headlamp.delete-resources',
+  'headlamp.create-resource',
+  'headlamp.edit-resource',
+  'headlamp.scale-resource',
+  'headlamp.restart-resource',
+  'headlamp.restart-resources',
+  'headlamp.rollback-resource',
+  'headlamp.logs',
+  'headlamp.terminal',
+  'headlamp.pod-attach',
+  'headlamp.plugin-loading-error',
+  'headlamp.details-view',
+  'headlamp.list-view',
+  'headlamp.object-events',
+  // PLUGINS_LOADED routes to trackPluginsLoaded; ERROR_BOUNDARY is captured
+  // by TelemetryErrorBoundary directly. Both intentionally omitted.
+]);
+
+export const EVENT_STATUSES: ReadonlySet<string> = new Set([
+  'unknown',
+  'open',
+  'closed',
+  'confirmed',
+  'finished',
+]);
+
+/**
+ * Azure region shape: lowercase letters containing a compass keyword and
+ * an optional 1-2 digit suffix. The `region` value flows from ARM, not
+ * user input, so a shape check is sufficient and immune to rot when
+ * Azure adds new regions.
+ */
+export const AZURE_REGION_RE = /^[a-z]*(central|north|south|east|west)[a-z]*\d{0,2}$/i;
+
+export function sanitizeKind(kind: string | undefined): string {
+  if (!kind) return 'Unknown';
+  // 'Multiple' is the fixed-vocabulary bucket sentinel returned by
+  // extractKindFromPayload for heterogeneous plural-resource events
+  // (delete-resources / restart-resources). Pass through verbatim so
+  // re-sanitization downstream doesn't clamp it to 'CustomResource'.
+  if (kind === 'Multiple') return 'Multiple';
+  return BUILTIN_K8S_KINDS.has(kind) ? kind : 'CustomResource';
+}
+
+export function sanitizeRegion(region: string | null | undefined): string {
+  if (!region) return 'Other';
+  return AZURE_REGION_RE.test(region) ? region.toLowerCase() : 'Other';
+}
+
+export function sanitizeFeatureType(type: string | undefined): string | undefined {
+  return type && KNOWN_FEATURE_TYPES.has(type) ? type : undefined;
+}
+
+export function sanitizeStatus(status: string | null | undefined): string {
+  if (!status) return 'unknown';
+  return EVENT_STATUSES.has(status) ? status : 'unknown';
+}
+
+export type AksTier = 'Free' | 'Standard' | 'Premium' | 'Unknown';
+const VALID_AKS_TIERS: ReadonlySet<string> = new Set(['Free', 'Standard', 'Premium']);
+
+export function sanitizeTier(tier: string | null | undefined): AksTier {
+  return tier && VALID_AKS_TIERS.has(tier) ? (tier as AksTier) : 'Unknown';
+}
+
+export type NodeCountBucket = '0' | '1-5' | '6-20' | '21-100' | '100+';
+export function bucketNodeCount(n: number): NodeCountBucket {
+  if (n <= 0) return '0';
+  if (n <= 5) return '1-5';
+  if (n <= 20) return '6-20';
+  if (n <= 100) return '21-100';
+  return '100+';
+}
+
+export type NamespaceCountBucket = '0' | '1-10' | '11-50' | '51-200' | '200+';
+export function bucketNamespaceCount(n: number): NamespaceCountBucket {
+  if (n <= 0) return '0';
+  if (n <= 10) return '1-10';
+  if (n <= 50) return '11-50';
+  if (n <= 200) return '51-200';
+  return '200+';
+}
+
+export function kubernetesMinor(version: string): string {
+  const m = /^v?(\d+)\.(\d+)/.exec(version);
+  return m ? `${m[1]}.${m[2]}` : 'unknown';
+}
+
+export function localeLanguage(locale: string): string {
+  if (!locale) return 'unknown';
+  return locale.split(/[-_]/)[0].toLowerCase();
+}

gambtho/aks-desktop

plugins/aks-desktop/src/telemetry/index.ts
@@ -1,0 +1,210 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache 2.0.
+
+import { ApplicationInsights, type ITelemetryItem } from '@microsoft/applicationinsights-web';
+import type { AppInfo } from './appInfo';
+import type { InstallId } from './installId';
+import {
+  type AksTier,
+  bucketNamespaceCount,
+  bucketNodeCount,
+  KNOWN_PLUGIN_IDS,
+  kubernetesMinor,
+  localeLanguage,
+  type NamespaceCountBucket,
+  type NodeCountBucket,
+  sanitizeFeatureType,
+  sanitizeKind,
+  sanitizeRegion,
+  sanitizeStatus,
+  sanitizeTier,
+} from './schema';
+import { registerReduxCallback } from './setup';
+
+// Module-private SDK handle. Only the typed track* functions below can
+// post envelopes.
+let ai: ApplicationInsights | null = null;
+
+// True once initTelemetry has run, even if construction failed. Lets us
+// skip retries on subsequent renders (e.g. StrictMode double-mount,
+// re-mount after a failed connection-string parse).
+let initAttempted = false;
+
+// Per-resource-id dedupe of headlamp.cluster-shape across re-renders.
+// Module-private (the key is never sent).
+const emittedShapeFor = new Set<string>();
+
+/** Test-only state reset. Do not call from production code. */
+export function __resetForTests(): void {
+  ai = null;
+  initAttempted = false;
+  emittedShapeFor.clear();
+}
+
+export interface SessionStartProps extends AppInfo {
+  appVersion: string;
+  /** Headlamp base version (REACT_APP_HEADLAMP_VERSION). */
+  headlampVersion: string;
+  locale: string;
+}
+
+export interface InitTelemetryOptions {
+  connectionString: string;
+  /** Anonymous per-install UUID from {@link getOrCreateInstallId}. Never pass PII here. */
+  installId: InstallId;
+  sessionProps: SessionStartProps;
+}
+
+/**
+ * Initialize App Insights and wire the redux event callback. Idempotent:
+ * the initAttempted flag survives React StrictMode double-mount.
+ *
+ * Fails closed — if the AI constructor throws, ai stays null and
+ * initAttempted stays true so we don't retry on every render.
+ */
+export function initTelemetry(opts: InitTelemetryOptions): void {
+  if (initAttempted) return;
+  initAttempted = true;
+
+  try {
+    ai = new ApplicationInsights({
+      config: {
+        connectionString: opts.connectionString,
+        disableFetchTracking: true,
+        disableAjaxTracking: true,
+        disableExceptionTracking: true,
+        disableCookiesUsage: true,
+        isStorageUseDisabled: true,
+        enableAutoRouteTracking: false,
+      },
+    });
+    ai.loadAppInsights();
+
+    // Stamp ai.user.id with the install UUID so the Azure Portal Users
+    // metric correlates by install rather than by SDK session. The id
+    // lives only as an envelope tag — never as a regular property
+    // dimension (it would otherwise be queryable/exportable as data).
+    const { installId } = opts;
+    ai.addTelemetryInitializer((envelope: ITelemetryItem) => {
+      envelope.tags = envelope.tags ?? {};
+      envelope.tags['ai.user.id'] = installId;
+    });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error('[aksd-telemetry] init failed:', e);
+    ai = null;
+    return;
+  }
+
+  trackSessionStart(opts.sessionProps);
+  registerReduxCallback();
+}
+
+export function isTelemetryAttempted(): boolean {
+  return initAttempted;
+}
+
+/** The only path from the typed wrappers to ai.trackEvent. */
+function emit(name: string, properties: Record<string, string | undefined>): void {
+  if (!ai) return;
+  const filtered: Record<string, string> = {};
+  for (const [key, value] of Object.entries(properties)) {
+    if (value !== undefined) filtered[key] = value;
+  }
+  try {
+    ai.trackEvent({ name, properties: filtered });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error('[aksd-telemetry] track failed:', name, e);
+  }
+}
+
+export function trackSessionStart(p: SessionStartProps): void {
+  emit('headlamp.session-start', {
+    appVersion: p.appVersion,
+    locale: localeLanguage(p.locale),
+    os: p.os,
+    arch: p.arch,
+    electronVersion: p.electronVersion,
+    headlampVersion: p.headlampVersion,
+  });
+}
+
+export interface ClusterShapeInput {
+  kubernetesVersion: string | null | undefined;
+  nodeCount: number | null | undefined; // 0 is valid data (not absent)
+  namespaceCount: number | null | undefined; // 0 is valid data (not absent)
+  region: string | null | undefined;
+  /** Raw ARM tier string — sanitized to AksTier on emit */
+  aksTier: string | null | undefined;
+}
+
+/**
+ * Emits one `headlamp.cluster-shape` per dedupeKey. No-ops when any
+ * field is null/undefined/empty or when dedupeKey was already seen --
+ * never mix Unknown with real values, never re-emit for the same
+ * cluster across re-renders or capability refreshes (once per session
+ * per resource-id, regardless of data changes).
+ */
+export function trackClusterShape(dedupeKey: string, input: ClusterShapeInput): void {
+  // Bail before touching the dedupe set when telemetry isn't initialized:
+  // otherwise a pre-init call would mark the key as seen and permanently
+  // suppress the post-init emission for the same cluster.
+  if (!ai || emittedShapeFor.has(dedupeKey)) return;
+  const {
+    kubernetesVersion: kv,
+    nodeCount: nc,
+    namespaceCount: nsc,
+    region: r,
+    aksTier: t,
+  } = input;
+  if (!kv || nc === null || nc === undefined || nsc === null || nsc === undefined || !r || !t) {
+    return;
+  }
+  emittedShapeFor.add(dedupeKey);
+  emit('headlamp.cluster-shape', {
+    provider: 'AKS',
+    kubernetesMinor: kubernetesMinor(kv),
+    nodeCountBucket: bucketNodeCount(nc) satisfies NodeCountBucket,
+    namespaceCountBucket: bucketNamespaceCount(nsc) satisfies NamespaceCountBucket,
+    region: sanitizeRegion(r),
+    aksTier: sanitizeTier(t) satisfies AksTier,
+  });
+}
+
+export interface FeatureProps {
+  feature: string;
+  status: string;
+  resourceKind?: string;
+}
+
+export function trackFeature(p: FeatureProps): void {
+  const safeFeature = sanitizeFeatureType(p.feature);
+  if (safeFeature === undefined) return;
+  emit('headlamp.feature', {
+    feature: safeFeature,
+    status: sanitizeStatus(p.status),
+    resourceKind: p.resourceKind === undefined ? undefined : sanitizeKind(p.resourceKind),
+  });
+}
+
+export function trackException(errorName: string): void {
+  emit('headlamp.exception', { errorName: errorName || 'Unknown' });
+}
+
+export interface PluginsLoadedProps {
+  totalCount: number;
+  enabledCount: number;
+  knownEnabledIds: string[];
+  thirdPartyCount: number;
+}
+
+export function trackPluginsLoaded(p: PluginsLoadedProps): void {
+  const knownOnly = p.knownEnabledIds.filter(id => KNOWN_PLUGIN_IDS.has(id));
+  emit('headlamp.plugins-loaded', {
+    totalCount: String(p.totalCount),
+    enabledCount: String(p.enabledCount),
+    knownEnabledIds: knownOnly.join(','),
+    thirdPartyCount: String(p.thirdPartyCount),
+  });
+}

plugins/aks-desktop/src/telemetry/schema.ts
@@ -1,0 +1,159 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache 2.0.
+
+/** Sanitizers and closed vocabularies for telemetry envelope properties. */
+
+export const BUILTIN_K8S_KINDS: ReadonlySet<string> = new Set([
+  // Core
+  'Pod',
+  'Service',
+  'Namespace',
+  'Node',
+  'ConfigMap',
+  'Secret',
+  'PersistentVolume',
+  'PersistentVolumeClaim',
+  'ServiceAccount',
+  'Endpoints',
+  'EndpointSlice',
+  'Event',
+  'LimitRange',
+  'ResourceQuota',
+  // Apps
+  'Deployment',
+  'StatefulSet',
+  'DaemonSet',
+  'ReplicaSet',
+  'ReplicationController',
+  // Batch
+  'Job',
+  'CronJob',
+  // Networking
+  'Ingress',
+  'IngressClass',
+  'NetworkPolicy',
+  'Gateway',
+  'HTTPRoute',
+  'GRPCRoute',
+  // Storage
+  'StorageClass',
+  'VolumeAttachment',
+  'CSIDriver',
+  'CSINode',
+  // RBAC
+  'Role',
+  'RoleBinding',
+  'ClusterRole',
+  'ClusterRoleBinding',
+  // Autoscaling
+  'HorizontalPodAutoscaler',
+  'VerticalPodAutoscaler',
+  'PodDisruptionBudget',
+  // Admission
+  'MutatingWebhookConfiguration',
+  'ValidatingWebhookConfiguration',
+  // API
+  'CustomResourceDefinition',
+  'APIService',
+  // Lease
+  'Lease',
+]);
+
+export const KNOWN_PLUGIN_IDS: ReadonlySet<string> = new Set(['aks-desktop']);
+
+/** Redux event types forwarded as `headlamp.feature` envelopes. */
+export const KNOWN_FEATURE_TYPES: ReadonlySet<string> = new Set([
+  'headlamp.delete-resource',
+  'headlamp.delete-resources',
+  'headlamp.create-resource',
+  'headlamp.edit-resource',
+  'headlamp.scale-resource',
+  'headlamp.restart-resource',
+  'headlamp.restart-resources',
+  'headlamp.rollback-resource',
+  'headlamp.logs',
+  'headlamp.terminal',
+  'headlamp.pod-attach',
+  'headlamp.plugin-loading-error',
+  'headlamp.details-view',
+  'headlamp.list-view',
+  'headlamp.object-events',
+  // PLUGINS_LOADED routes to trackPluginsLoaded; ERROR_BOUNDARY is captured
+  // by TelemetryErrorBoundary directly. Both intentionally omitted.
+]);
+
+export const EVENT_STATUSES: ReadonlySet<string> = new Set([
+  'unknown',
+  'open',
+  'closed',
+  'confirmed',
+  'finished',
+]);
+
+/**
+ * Azure region shape: lowercase letters containing a compass keyword and
+ * an optional 1-2 digit suffix. The `region` value flows from ARM, not
+ * user input, so a shape check is sufficient and immune to rot when
+ * Azure adds new regions.
+ */
+export const AZURE_REGION_RE = /^[a-z]*(central|north|south|east|west)[a-z]*\d{0,2}$/i;
+
+export function sanitizeKind(kind: string | undefined): string {
+  if (!kind) return 'Unknown';
+  // 'Multiple' is the fixed-vocabulary bucket sentinel returned by
+  // extractKindFromPayload for heterogeneous plural-resource events
+  // (delete-resources / restart-resources). Pass through verbatim so
+  // re-sanitization downstream doesn't clamp it to 'CustomResource'.
+  if (kind === 'Multiple') return 'Multiple';
+  return BUILTIN_K8S_KINDS.has(kind) ? kind : 'CustomResource';
+}
+
+export function sanitizeRegion(region: string | null | undefined): string {
+  if (!region) return 'Other';
+  return AZURE_REGION_RE.test(region) ? region.toLowerCase() : 'Other';
+}
+
+export function sanitizeFeatureType(type: string | undefined): string | undefined {
+  return type && KNOWN_FEATURE_TYPES.has(type) ? type : undefined;
+}
+
+export function sanitizeStatus(status: string | null | undefined): string {
+  if (!status) return 'unknown';
+  return EVENT_STATUSES.has(status) ? status : 'unknown';
+}
+
+type KnownAksTier = 'Free' | 'Standard' | 'Premium';
+export type AksTier = KnownAksTier | 'Unknown';
+const VALID_AKS_TIERS: ReadonlySet<KnownAksTier> = new Set(['Free', 'Standard', 'Premium']);
+
+export function sanitizeTier(tier: string | null | undefined): AksTier {
+  return tier && VALID_AKS_TIERS.has(tier as KnownAksTier) ? (tier as KnownAksTier) : 'Unknown';
+}
+
+export type NodeCountBucket = '0' | '1-5' | '6-20' | '21-100' | '100+';
+export function bucketNodeCount(n: number): NodeCountBucket {
+  if (n <= 0) return '0';
+  if (n <= 5) return '1-5';
+  if (n <= 20) return '6-20';
+  if (n <= 100) return '21-100';
+  return '100+';
+}
+
+export type NamespaceCountBucket = '0' | '1-10' | '11-50' | '51-200' | '200+';
+export function bucketNamespaceCount(n: number): NamespaceCountBucket {
+  if (n <= 0) return '0';
+  if (n <= 10) return '1-10';
+  if (n <= 50) return '11-50';
+  if (n <= 200) return '51-200';
+  return '200+';
+}
+
+export function kubernetesMinor(version: string): string {
+  const m = /^v?(\d+)\.(\d+)/.exec(version);
+  return m ? `${m[1]}.${m[2]}` : 'unknown';
+}
+
+export function localeLanguage(locale: string): string {
+  if (!locale) return 'unknown';
+  return locale.split(/[-_]/)[0].toLowerCase();
+}